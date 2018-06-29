aa9606021fb96e7514144ecaf55b2f7fdd3a7422
Marko Madic <marko.madic@congility.com>
2018-06-30 01:59:13 +0200

    Added changes for Bonus zone

--- src/game/loop/zones.js
+++ src/game/loop/zones.js
@@ -1,12 +1,13 @@
 import {getHtmlColor} from '../../scene';
 import {DirMode} from '../../game/actors';
+import {loadSprite} from '../../iso/sprites';
 
 export const ZoneOpcode = [
     { opcode: 0, command: 'CUBE', callback: CUBE },
     { opcode: 1, command: 'CAMERA', callback: NOP },
     { opcode: 2, command: 'SCENERIC', callback: NOP },
     { opcode: 3, command: 'FRAGMENT', callback: NOP },
-    { opcode: 4, command: 'BONUS', callback: NOP },
+    { opcode: 4, command: 'BONUS', callback: BONUS },
     { opcode: 5, command: 'TEXT', callback: TEXT },
     { opcode: 6, command: 'LADDER', callback: NOP },
     { opcode: 7, command: 'CONVEYOR', callback: NOP },
@@ -100,6 +101,62 @@ function TEXT(game, scene, zone, hero) {
         window.removeEventListener('keydown', scene.zoneState.listener);
         delete scene.zoneState.listener;
         delete scene.zoneState.ended;
+    }
+}
+
+function BONUS(game, scene, zone, hero) {
+    if (game.controlsState.action === 1) {
+        hero.props.prevEntityIndex = hero.props.entityIndex;
+        hero.props.prevAnimIndex = hero.props.animIndex;
+        hero.props.entityIndex = 0;
+
+        // CHECK Possibly needs to use 'waitPosition' runtime flag
+        hero.props.runtimeFlags.isDoingBonus = true;
+
+        hero.props.animIndex = 11; // Action jump, equivalent to real-index 17
+
+        const base2 = (zone.props.info0).toString(2);
+        const binaryTypes = ('000000000').substr(base2.length) + base2;
+        const typesArray = [];
+
+        /* TODO
+         * Consider an object type or class for bonus items, instead of just scene sprites
+         * It probably needs to have a type of its own in order to be able to move/animate,
+         * and be updated from main loop
+         */
+
+        // Populate array of possible spriteTypes based on the binary value of the info0 prop
+        for (let i = binaryTypes.length; i > 0; i -= 1) {
+            if (parseInt(binaryTypes[binaryTypes.length - i], 10) === 1)
+                typesArray.push(i);
+        }
+
+        // Pick one of the available spriteTypes (multiple can be set) at random from the Array
+        let spriteIndex = -2 + typesArray[Math.floor(Math.random() * typesArray.length)];
+
+        /* TODO
+         * Decide which planet you're on, in order to decide what type of money to spawn. (sprite)
+         * Current sprite value for gold is "3", which is a pointer according to kazekr website.
+         * Check GIVE_BONUS, GIVE_GOLD_PIECES or similar scripts.
+        */
+
+        switch (spriteIndex) {
+            case 3:
+                // In case of gold, pick Kashes for now
+                spriteIndex += 15;
+                break;
+        }
+
+        if (zone.props.snap === 0) {
+            // If it is an Outside scene, use a bilboarded Three.Sprite.
+            // Otherwise, use a Three.Mesh
+            loadSprite(spriteIndex, (bonusSprite) => {
+                bonusSprite.threeObject.position.copy(zone.physics.position);
+                scene.addMesh(bonusSprite.threeObject);
+            }, scene.data.isOutsideScene);
+
+            zone.props.snap = 1;
+        }
     }
 }
 
